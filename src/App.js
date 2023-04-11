de0a68c209df1d353b015c0091d650ceac27bde3
HANJIYEONN <onedelay1@naver.com>
2023-04-11 23:01:56 +0900

    Feat: 1. 페이지 나누는 법 2. 리액트 라우터 설치 3. 페이지 이동 버튼

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ import bg from './bg.jpg'
 import React, { useState } from 'react';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import data from './data.js';
+import { Routes, Route, Link } from "react-router-dom";
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
-const ColComponent = (props) => {
-  return (
-    <div className="col-md-4">
-      <img src={props.shoes.url} width="80%"/>
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.content}</p>
-    </div>
-  )
-}
-
-
 function App() {
+  
   let [shoes] = useState(data);
 
   return (
@@ -28,33 +19,47 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            {/* <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link> */}
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Detail</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg" style={{ backgroundImage : 'url('+ bg +')'}}></div>
-
-      <div className="container">
-        <div className="row">
-        
-        {
-          shoes.map((a,i)=>{
-            return (
-              <ColComponent shoes = {shoes[i]}/> 
-            )
-          })
-        }
-        </div>
-      </div>
-
-      <Button as="a" variant="success">
-        Button as link
-      </Button>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg" style={{ backgroundImage : 'url('+ bg +')'}}></div>
+            <div className="container">
+              <div className="row">
+              {
+                shoes.map((a,i)=>{
+                  return (
+                    <ColComponent shoes = {shoes[i]}/> 
+                  )
+                })
+              }
+              </div>
+            </div>
+          </>
+        }/>
+        <Route path="/detail" element={<div>상세페이지임</div>}/>
+        <Route path="/about" element={<div>어바웃페이지임</div>}/>
+      </Routes>
     </div>
   );
+}
+
+const ColComponent = (props) => {
+  return (
+    <div className="col-md-4">
+      <img src={props.shoes.url} width="80%"/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+    </div>
+  )
 }
 
 export default App;
